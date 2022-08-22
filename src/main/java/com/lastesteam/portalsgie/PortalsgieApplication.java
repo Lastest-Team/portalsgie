03fbbb680132068b5e6b7da2e26f6aad8706a031
Jose Alejandro Gonzalez Ortiz <70743629+JoseGonzalez-git@users.noreply.github.com>
2022-08-21 20:28:52 -0500

    Inicializando proyecto

---
+++ src/main/java/com/lastesteam/portalsgie/PortalsgieApplication.java
@@ -0,0 +1,13 @@
+package com.lastesteam.portalsgie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class PortalsgieApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(PortalsgieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastesteam/portalsgie/PortalsgieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastesteam.portalsgie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalsgieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
