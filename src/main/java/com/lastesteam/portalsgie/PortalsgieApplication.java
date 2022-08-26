5c440d3f9d20f396533ab423f0436b9b2812bd90
Jose Alejandro Gonzalez Ortiz <70743629+JoseGonzalez-git@users.noreply.github.com>
2022-08-26 07:58:15 -0500

    Update PortalsgieApplication.java
    Inicializando usuarios

--- src/main/java/com/lastesteam/portalsgie/PortalsgieApplication.java
+++ src/main/java/com/lastesteam/portalsgie/PortalsgieApplication.java
@@ -6,13 +6,16 @@ public class PortalsgieApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(PortalsgieApplication.class, args);
 		User user1 = new User(12,"Jhon Doe","jhond","jhond@correo.com","qwerty");
-		Transaction Transaction = new Transaction(2222,"Nomina",2.33,user1);
-		Transaction.showMovementMoney();
+		user1.printUser();
+		user1.setName("Juan G");
+		user1.printUser();
+		User user2 = new User(12,"Jhon K","jhond","jhond@correo.com","qwerty");
+		user2.printUser();
+		//Transaction Transaction = new Transaction(2222,"Nomina",2.33,user1);
+		//Transaction.showMovementMoney();
 		//Enterprise Enterprise = new Enterprise(/*name: address: phone: document:*/ );
 		//Enterprise.showEnterprise();
-
-
-/*
+		/*
 		//SpringApplication.run(PortalsgieApplication.class, args);
 		User user1 = new User(12,"Jhon Doe","jhond","jhond@correo.com","qwerty");
 		Transaction Transaction = new Transaction(2222,"Nomina",2.33,user1);
