e443792fd7cdecb402ffac31f88f85c95f0ad6cb
waalzate <waalzate@gmail.com>
2022-08-31 14:23:19 -0500

    Se crea el paquete entities

--- src/main/java/com/lastesteam/portalsgie/PortalsgieApplication.java
+++ src/main/java/com/lastesteam/portalsgie/PortalsgieApplication.java
@@ -33,7 +33,6 @@ public class PortalsgieApplication {
 		//SpringApplication.run(PortalsgieApplication.class, args);
 		User user1 = new User(12,"Jhon Doe","jhond","jhond@correo.com","qwerty");
 		Transaction Transaction = new Transaction(2222,"Nomina",2.33,user1);
-
 		Enterprise Enterprise1 = new Enterprise(name: address: phone: document: );
 		Enterprise.showEnterprise();  */
 	}
